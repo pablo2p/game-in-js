a953da8cefee9046df090a099783978ec8d7b3ed
Pablo <zkyouta@hotmail.com>
2020-06-27 18:42:35 -0300

    Colisão + Pulos

---
+++ scripts/jogo/inimigo.js
@@ -0,0 +1,17 @@
+class Inimigo extends Animation {
+    constructor(matriz, imagem, x, largura, altura,
+        larguraSprite, alturaSprite) {
+          super(matriz, imagem, x, largura, altura,
+            larguraSprite, alturaSprite);  
+
+            this.velocidade = 10;
+    }
+
+    move(){
+        this.x = this.x - this.velocidade;
+
+        if (this.x < -this.largura){
+            this.x = width;
+        }
+    }
+}
